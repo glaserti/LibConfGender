802727d5cb98b6d77c31b686192a79fb32d11db6
glaserti <timo.glaser@icloud.com>
2014-09-23 15:28:04 -0400

    added print(matr_perc)-command

--- visualizeSpeakerStats.R
+++ visualizeSpeakerStats.R
@@ -20,16 +20,17 @@ percentagesFemale <- confSpeak$female * 100 / sumPerYear
 percentagesUndef <- confSpeak$undef * 100 / sumPerYear
 percentages <- c(percentagesMale, percentagesFemale, percentagesUndef)
 
+matr_perc <- matrix(c(percentagesFemale, percentagesMale, percentagesUndef),
+                    byrow=TRUE, nrow=3,
+                    dimnames = list(c('female', 'male', 'undef'),
+                                    c('2009', '2010', '2011', '2012', '2013', '2014')))
+print(round(matr_perc,2))
 
 # Plotting the total numbers
 barplot(matr_abs, main = "Präsentationen von Frauen/Männern 2009 - 2014",
         col = c('red3','dodgerblue3', 'limegreen'))
 
 # Plotting the percentages
-matr_perc <- matrix(c(percentagesFemale, percentagesMale, percentagesUndef),
-               byrow=TRUE, nrow=3,
-               dimnames = list(c('female', 'male', 'undef'),
-                               c('2009', '2010', '2011', '2012', '2013', '2014')))
 barplot(matr_perc, main = "Prozentuale Verteilung Frauen/Männer 2009 - 2014",
         col = c('red3','dodgerblue3', 'limegreen'))
 abline(h=50, lty = 5, lwd = 3)
